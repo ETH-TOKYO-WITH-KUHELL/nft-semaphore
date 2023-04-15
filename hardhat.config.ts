860aab1c861e27b4a12fe15550a985be09f5de16
ByeongHunKim <rkwhr912@naver.com>
2023-04-15 22:01:34 +0900

    edit : hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const MUMBAI_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     mumbai:{
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      chainId: 80001,
       accounts: [MUMBAI_PRIVATE_KEY],
     },
   }
